9b2f2d95a60f9059aa152da5357f043c1be8dc50
Lunarmagpie <Bambolambo0@gmail.com>
2022-01-23 17:01:46 -0500

    :art: update banner

--- crescent/__init__.py
+++ crescent/__init__.py
@@ -5,6 +5,12 @@ from crescent.mentionable import *
 
 from typing import Sequence
 
+from hikari import _about
+
+_about.__maintainer__ = "Lunarmagpie"
+_about.__copyright__ = "© 2021 Lunarmagpie"
+_about.__license__ = "MPL 2.0"
+
 __all__: Sequence[str] = (
     "Description",
     "Name",
