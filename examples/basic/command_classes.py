de7165051a8de9f698eb684cc3c61fab10384828
Lunarmagpie <Bambolambo0@gmail.com>
2022-01-31 20:14:39 -0500

    add name back to example

--- examples/basic/command_classes.py
+++ examples/basic/command_classes.py
@@ -11,7 +11,7 @@ bot = crescent.Bot(token="...")
 @bot.include
 @crescent.command(name="echo")
 class Say:
-    to_say = crescent.option(str, "Make the bot say something", default="...")
+    to_say = crescent.option(str, "Make the bot say something", default="...", name="to-say")
     channel = crescent.option(hikari.GuildTextChannel, "The channel to send in", default=None)
 
     async def callback(self, ctx: crescent.Context) -> None:
