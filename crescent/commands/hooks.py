bfc2899dc79a9f85f49c596d1fccc29eadc6284a
Circuit <circuitsacul@icloud.com>
2022-02-04 09:23:41 -0500

    :bug: Fix hook decorator

--- crescent/commands/hooks.py
+++ crescent/commands/hooks.py
@@ -36,14 +36,10 @@ def hook(callback: HookCallbackT, command: T, /) -> T:
     ...
 
 
-def hook(callback, command=None):
-
+def hook(callback: HookCallbackT, command: T | None = None) -> T | Callable[..., T]:
     if command is None:
-        return partial(hook, callback)
-
+        return partial(hook, callback)  # type: ignore
     if not iscoroutinefunction(callback):
         raise ValueError(f"Function `{callback.__name__}` must be async.")
-
-    command.interaction_hooks.insert(0, callback)
-
+    command.metadata.hooks.insert(0, callback)
     return command
