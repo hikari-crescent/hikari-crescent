5ea1f15c58460575c8592a6d50127a52e0ec2921
Circuit <circuitsacul@icloud.com>
2022-02-09 12:13:02 -0500

    More testing stuff

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,16 +1,47 @@
 import os
 
 import dotenv
+import hikari
 
 import crescent
 
 dotenv.load_dotenv()
 
 
-bot = crescent.Bot(os.getenv("TOKEN"), default_guild=int(os.getenv("GUILD")))
+async def myhook(ctx: crescent.Context) -> None:
+    await ctx.respond("Hook Called", ephemeral=True)
 
 
-group = crescent.Group("group")
+class Bot(crescent.Bot):
+    @crescent.user_command(name="Subclassed User")
+    async def sc_user(self, ctx: crescent.Context, user: hikari.User):
+        assert isinstance(self, Bot)
+        await ctx.respond(str(user))
+
+    @crescent.message_command(name="Subclassed Message")
+    async def sc_msg(self, ctx: crescent.Context, msg: hikari.Message):
+        assert isinstance(self, Bot)
+        await ctx.respond(str(msg))
+
+    @crescent.hook(myhook)
+    @crescent.command
+    async def subclassed_command(self, ctx: crescent.Context) -> None:
+        assert isinstance(self, Bot)
+        await ctx.respond("works")
+
+    async def on_crescent_error(
+        self, exc: Exception, ctx: crescent.Context, was_handled: bool
+    ) -> None:
+        await ctx.respond(
+            f"default err handler called. was_handled={was_handled}"
+        )
+        return await super().on_crescent_error(exc, ctx, was_handled)
+
+
+bot = Bot(os.getenv("TOKEN"), default_guild=int(os.getenv("GUILD")))
+
+
+group = crescent.Group("group", hooks=[myhook])
 subgroup = group.sub_group("sub")
 
 
@@ -39,9 +70,44 @@ async def group_command(ctx: crescent.Context, arg: str) -> None:
 
 @bot.include
 @subgroup.child
+@crescent.hook(myhook)
 @crescent.command
 async def subgroup_command(ctx: crescent.Context, arg: str) -> None:
     await ctx.respond(arg)
+
+
+@bot.include
+@crescent.user_command(name="User")
+async def user_command(ctx: crescent.Context, user: hikari.User) -> None:
+    await ctx.respond(str(user))
+
+
+@bot.include
+@crescent.message_command(name="Message")
+async def msg_command(ctx: crescent.Context, msg: hikari.Message) -> None:
+    await ctx.respond(str(msg))
+
+
+class Err(Exception):
+    pass
+
+
+@bot.include
+@crescent.catch(Err)
+async def handle_err(exc: Err, ctx: crescent.Context) -> None:
+    await ctx.respond(str(exc))
+
+
+@bot.include
+@crescent.command
+async def raise_err(ctx: crescent.Context) -> None:
+    raise Err("test")
+
+
+@bot.include
+@crescent.command
+async def raise_unhandled_err(ctx: crescent.Context) -> None:
+    raise Exception("test")
 
 
 bot.run()
---
+++ tests/test_plugin.py
@@ -0,0 +1,10 @@
+import crescent
+
+
+plugin = crescent.Plugin("plugin")
+
+
+@plugin.include
+@crescent.command
+async def plugin_cmd(ctx: crescent.Context) -> None:
+    await ctx.respond("plugins work")
