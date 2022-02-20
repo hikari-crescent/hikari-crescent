c148bf8ef3b497f6ac13e8439fc746cfb1095f10
Lunarmagpie <Bambolambo0@gmail.com>
2022-02-19 23:29:58 -0500

    :bug: fix dumb bug with signature parsing

--- crescent/commands/args.py
+++ crescent/commands/args.py
@@ -14,7 +14,7 @@ class Arg(ABC):
         ...
 
 
-@define
+@define(hash=True)
 class Description(Arg):
     description: str
 
@@ -23,7 +23,7 @@ class Description(Arg):
         return self.description
 
 
-@define
+@define(hash=True)
 class Name(Arg):
     name: str
 
@@ -50,7 +50,7 @@ class ChannelTypes(Arg):
         return self.channel_types
 
 
-@define
+@define(hash=True)
 class MaxValue(Arg):
     max_value: int
 
@@ -59,7 +59,7 @@ class MaxValue(Arg):
         return self.max_value
 
 
-@define
+@define(hash=True)
 class MinValue(Arg):
     min_value: int
 
