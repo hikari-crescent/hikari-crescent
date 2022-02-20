3eadd9d43a09a3f172f81b913a878fde1b66a369
Lunarmagpie <Bambolambo0@gmail.com>
2022-02-19 21:15:09 -0500

    :sparkles: add update_commands kwarg (#85)
    * :sparkles: add update_commands kwarg
    
    * :memo: Autogenerated docs
    
    * update the tests
    
    * update the tests
    
    * change typing
    
    * :memo: Autogenerated docs
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: Github Actions <actions@magpie.dev>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/crescent/internal/test_registry.py
+++ tests/crescent/internal/test_registry.py
@@ -1,4 +1,6 @@
-from unittest.mock import AsyncMock, MagicMock
+from collections import defaultdict
+from contextlib import ExitStack
+from unittest.mock import AsyncMock, MagicMock, patch
 
 from hikari import Message, User
 from hikari.impl import CacheImpl, RESTClientImpl
@@ -7,6 +9,7 @@ from pytest import fixture, mark
 from crescent import Context, command
 from crescent import message_command as _message_command
 from crescent import user_command as _user_command
+from crescent.internal.registry import CommandHandler
 from tests.utils import MockBot
 
 GUILD_ID = 123456789
@@ -15,7 +18,7 @@ GUILD_ID = 123456789
 class TestRegistry:
     @fixture(autouse=True)
     def mock_send(self):
-        self.posted_commands = {}
+        self.posted_commands = defaultdict(list)
 
         def set_application_commands(application, commands, guild=None):
             self.posted_commands[guild] = commands
@@ -44,9 +47,8 @@ class TestRegistry:
         async def message_command(ctx: Context, message: Message):
             pass
 
-        bot._command_handler.application_id = ...
-
-        await bot._command_handler.register_commands()
+        bot.run()
+        await bot.wait_until_ready()
 
         assert self.posted_commands[GUILD_ID] == [
             slash_command.metadata.app,
@@ -88,10 +90,40 @@ class TestRegistry:
         async def message_command(ctx: Context, message: Message):
             pass
 
-        await bot._command_handler.register_commands()
+        bot.run()
+        await bot.wait_until_ready()
+        print(self.posted_commands[GUILD_ID])
 
         assert self.posted_commands[GUILD_ID] == [
             slash_command.metadata.app,
             user_command.metadata.app,
             message_command.metadata.app,
         ]
+
+    @mark.asyncio
+    async def test_dont_register_commands(self):
+        stack = ExitStack()
+        register_commands = stack.enter_context(patch.object(CommandHandler, "register_commands"))
+
+        bot = MockBot(default_guild=GUILD_ID, update_commands=False)
+
+        @bot.include
+        @command
+        async def slash_command(ctx: Context):
+            pass
+
+        @bot.include
+        @_user_command
+        async def user_command(ctx: Context, user: User):
+            pass
+
+        @bot.include
+        @_message_command
+        async def message_command(ctx: Context, message: Message):
+            pass
+
+        bot.run()
+        await bot.wait_until_ready()
+
+        register_commands.assert_not_called()
+        assert self.posted_commands[GUILD_ID] == []
