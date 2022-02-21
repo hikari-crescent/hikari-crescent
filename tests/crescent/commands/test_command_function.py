cb4f00604a8fdb632719cd8768d1fd99ec62c501
Lunarmagpie <Bambolambo0@gmail.com>
2022-02-20 22:45:55 -0500

    :bug: fix bug with signature parsing (#87)
    * :bug: fix dumb bug with signature parsing
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * :memo: Autogenerated docs
    
    * fix hash
    
    * :memo: Autogenerated docs
    
    * write custom hash function
    
    * remove bitshift
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Github Actions <actions@magpie.dev>

--- tests/crescent/commands/test_command_function.py
+++ tests/crescent/commands/test_command_function.py
@@ -1,76 +1,140 @@
 from typing import Optional
 
-from hikari import UNDEFINED, CommandOption, CommandType, Message, OptionType, User
-from typing_extensions import Annotated as Atd
+from hikari import (
+    UNDEFINED,
+    ChannelType,
+    CommandOption,
+    CommandType,
+    Message,
+    OptionType,
+    PartialChannel,
+    User,
+)
+from typing_extensions import Annotated
 
-from crescent import Context, command, message_command, user_command
+from crescent import Context, Name, command, message_command, user_command
+from crescent.commands.args import ChannelTypes, MaxValue, MinValue
 from crescent.internal.app_command import AppCommand, AppCommandMeta
 
 
-def test_command_function():
-    @command(description="1234")
-    async def callback(
-        ctx: Context,
-        arg_1: Atd[str, "1234"],
-        arg_2: Atd[Optional[str], "1234"] = None,
-        arg_3: Optional[float] = None,
-    ):
-        pass
+class TestCommandFunction:
+    def test_slash_command(self):
+        @command(name="test", description="1234", guild=12345678)
+        async def callback(ctx: Context):
+            pass
 
-    assert callback.metadata == AppCommandMeta(
-        app=AppCommand(
-            type=CommandType.SLASH,
-            name="callback",
-            guild_id=None,
-            default_permission=UNDEFINED,
-            description="1234",
-            options=[
-                CommandOption(
-                    type=OptionType.STRING, name="arg_1", description="1234", is_required=True
-                ),
-                CommandOption(
-                    type=OptionType.STRING, name="arg_2", description="1234", is_required=False
-                ),
-                CommandOption(
-                    type=OptionType.FLOAT,
-                    name="arg_3",
-                    description="No Description",
-                    is_required=False,
-                ),
-            ],
+        assert callback.metadata == AppCommandMeta(
+            app=AppCommand(
+                type=CommandType.SLASH,
+                name="test",
+                guild_id=12345678,
+                default_permission=UNDEFINED,
+                description="1234",
+            )
         )
-    )
 
-    @command(name="test", description="1234", guild=12345678)
-    async def callback(ctx: Context):
-        pass
+    def test_annotated_command(self):
+        @command(description="1234")
+        async def callback(
+            ctx: Context,
+            arg_1: Annotated[str, "1234"],
+            arg_2: Annotated[Optional[str], "1234"] = None,
+            arg_3: Optional[float] = None,
+        ):
+            pass
 
-    assert callback.metadata == AppCommandMeta(
-        app=AppCommand(
-            type=CommandType.SLASH,
-            name="test",
-            guild_id=12345678,
-            default_permission=UNDEFINED,
-            description="1234",
+        assert callback.metadata == AppCommandMeta(
+            app=AppCommand(
+                type=CommandType.SLASH,
+                name="callback",
+                guild_id=None,
+                default_permission=UNDEFINED,
+                description="1234",
+                options=[
+                    CommandOption(
+                        type=OptionType.STRING, name="arg_1", description="1234", is_required=True
+                    ),
+                    CommandOption(
+                        type=OptionType.STRING, name="arg_2", description="1234", is_required=False
+                    ),
+                    CommandOption(
+                        type=OptionType.FLOAT,
+                        name="arg_3",
+                        description="No Description",
+                        is_required=False,
+                    ),
+                ],
+            )
         )
-    )
 
-    @message_command
-    async def callback(ctx: Context, message: Message):
-        pass
+    def test_crescent_annotations(self):
+        @command(description="1234")
+        async def callback(
+            ctx: Context,
+            test_name: Annotated[str, "1234", Name("name_override")],
+            test_min_and_max: Annotated[int, "1234", MinValue(0), MaxValue(10)] = None,
+            test_channels: Annotated[
+                PartialChannel, "1234", ChannelTypes(ChannelType.GUILD_TEXT)
+            ] = None,
+        ):
+            pass
 
-    assert callback.metadata == AppCommandMeta(
-        app=AppCommand(
-            type=CommandType.MESSAGE, name="callback", default_permission=UNDEFINED, guild_id=None
+        print(callback.metadata.app.options[2].channel_types)
+
+        assert callback.metadata == AppCommandMeta(
+            app=AppCommand(
+                type=CommandType.SLASH,
+                name="callback",
+                default_permission=UNDEFINED,
+                description="1234",
+                guild_id=None,
+                options=[
+                    CommandOption(
+                        type=OptionType.STRING,
+                        name="name_override",
+                        description="1234",
+                        is_required=True,
+                    ),
+                    CommandOption(
+                        type=OptionType.INTEGER,
+                        name="test_min_and_max",
+                        description="1234",
+                        is_required=False,
+                        min_value=0,
+                        max_value=10,
+                    ),
+                    CommandOption(
+                        type=OptionType.CHANNEL,
+                        name="test_channels",
+                        description="1234",
+                        is_required=False,
+                        channel_types=[ChannelType.GUILD_TEXT],
+                    ),
+                ],
+            )
         )
-    )
 
-    @user_command
-    async def callback(ctx: Context, user: User):
-        pass
+    def test_message_command(self):
+        @message_command
+        async def callback(ctx: Context, message: Message):
+            pass
 
-    assert callback.metadata == AppCommandMeta(
-        app=AppCommand(
-            type=CommandType.USER, name="callback", default_permission=UNDEFINED, guild_id=None
+        assert callback.metadata == AppCommandMeta(
+            app=AppCommand(
+                type=CommandType.MESSAGE,
+                name="callback",
+                default_permission=UNDEFINED,
+                guild_id=None,
+            )
         )
-    )
+
+    def test_user_command(self):
+        @user_command
+        async def callback(ctx: Context, user: User):
+            pass
+
+        assert callback.metadata == AppCommandMeta(
+            app=AppCommand(
+                type=CommandType.USER, name="callback", default_permission=UNDEFINED, guild_id=None
+            )
+        )
