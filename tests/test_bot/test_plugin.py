bbd04d0bf8ef986a0da62dbd0561ef1e1f69c88a
Circuit <circuitsacul@icloud.com>
2022-02-28 19:36:10 -0500

    Update test_plugin.py

--- tests/test_bot/test_plugin.py
+++ tests/test_bot/test_plugin.py
@@ -5,10 +5,15 @@ async def plugin_hook(ctx: crescent.Context) -> None:
     await ctx.respond("Plugin wide hook called.")
 
 
+async def command_hook(ctx: crescent.Context) -> None:
+    await ctx.respond("Command hook called.")
+
+
 plugin = crescent.Plugin("plugin", [plugin_hook])
 
 
 @plugin.include
+@crescent.hook(command_hook)
 @crescent.command
 async def plugin_cmd(ctx: crescent.Context) -> None:
     await ctx.respond("plugins work")
