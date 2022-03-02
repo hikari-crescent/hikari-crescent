d34228cef5007202a0b751b17fc5970a5322f8bb
Circuit <circuitsacul@icloud.com>
2022-03-01 20:03:44 -0500

    :sparkles: Bot-wide and plugin-wide hooks (#91)
    * Allow for plugin-wide hooks
    
    * :memo: Autogenerated docs
    
    * Don't require plugin hooks to be passed
    
    * :memo: Autogenerated docs
    
    * Add bot-wide hooks
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * :memo: Autogenerated docs
    
    * Fix useless f-string
    
    * Add bot-wide hooks to subclassed bot commands
    
    * Update test_plugin.py
    
    * :memo: Autogenerated docs
    
    * Fix hook ordering of plugin.include
    
    * Unit testing for hooks
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * :memo: Autogenerated docs
    
    * Change order of comparison
    
    Co-authored-by: Lunarmagpie <Bambolambo0@gmail.com>
    
    * Remove whitespace, fix imports
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: Github Actions <actions@magpie.dev>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Lunarmagpie <Bambolambo0@gmail.com>

--- crescent/plugin.py
+++ crescent/plugin.py
@@ -3,10 +3,13 @@ from __future__ import annotations
 from importlib import import_module
 from typing import TYPE_CHECKING, Dict, Tuple
 
+from crescent.internal.app_command import AppCommandMeta
 from crescent.internal.meta_struct import MetaStruct
 
 if TYPE_CHECKING:
     from typing import Sequence, TypeVar
+
+    from crescent.typedefs import HookCallbackT
 
     from .bot import Bot
 
@@ -48,20 +51,27 @@ class PluginManager:
 
 
 class Plugin:
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, command_hooks: list[HookCallbackT] | None = None) -> None:
         self.name = name
+        self.command_hooks = command_hooks
         self._children: list[Tuple[MetaStruct, bool]] = []
 
         for value in vars(self.__class__).values():
             if isinstance(value, MetaStruct):
+                if isinstance(value.metadata, AppCommandMeta) and self.command_hooks:
+                    value.metadata.hooks.extend(self.command_hooks)
                 self._children.append((value, True))
 
     def include(self, obj: T) -> T:
+        if isinstance(obj.metadata, AppCommandMeta) and self.command_hooks:
+            obj.metadata.hooks.extend(self.command_hooks)
         self._children.append((obj, False))
         return obj
 
     def _setup(self, bot: Bot) -> None:
         for item, is_method in self._children:
+            if isinstance(item.metadata, AppCommandMeta) and bot.command_hooks:
+                item.metadata.hooks.extend(bot.command_hooks)
             item.register_to_app(bot, self if is_method else None)
 
     @classmethod
