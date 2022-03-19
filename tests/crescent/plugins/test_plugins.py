40625101fec5a2ea0ff403c0534c924f6d9b04df
Circuit <circuitsacul@icloud.com>
2022-03-19 12:07:40 -0400

    Write tests for plugin loading/reloading

--- tests/crescent/plugins/test_plugins.py
+++ tests/crescent/plugins/test_plugins.py
@@ -1,8 +1,24 @@
+from __future__ import annotations
+
 from tests.utils import MockBot
+from tests.crescent.plugins.plugin import plugin as real_plugin
 
 
-class TestPlugins:
-    def test_load_plugin(self):
-        bot = MockBot()
+BOT = MockBot()
 
-        bot.plugins.load("tests.crescent.plugins.plugin")
+
+def test_load_plugin():
+    plugin = BOT.plugins.load("tests.crescent.plugins.plugin")
+    assert plugin is real_plugin
+    del BOT.plugins.plugins[real_plugin.name]
+
+
+def test_plugin_reload():
+    orig = BOT.plugins.load("tests.crescent.plugins.plugin")
+    del BOT.plugins.plugins[real_plugin.name]
+    orig2 = BOT.plugins.load("tests.crescent.plugins.plugin")
+
+    new = BOT.plugins.load("tests.crescent.plugins.plugin", refresh=True)
+
+    assert orig is orig2
+    assert orig is not new
