1626e701ec9f85cff74722be09d30b78108ff391
Circuit <circuitsacul@icloud.com>
2022-03-19 14:55:32 -0400

    Put plugin tests inside class

--- tests/crescent/plugins/test_plugins.py
+++ tests/crescent/plugins/test_plugins.py
@@ -4,21 +4,21 @@ from tests.crescent.plugins.plugin import plugin as real_plugin
 from tests.utils import MockBot
 
 
-def test_load_plugin():
-    bot = MockBot()
+class TestPlugins:
+    def test_load_plugin(self):
+        bot = MockBot()
 
-    plugin = bot.plugins.load("tests.crescent.plugins.plugin")
+        plugin = bot.plugins.load("tests.crescent.plugins.plugin")
 
-    assert plugin is real_plugin
+        assert plugin is real_plugin
 
+    def test_plugin_reload(self):
+        bot = MockBot()
 
-def test_plugin_reload():
-    bot = MockBot()
+        orig = bot.plugins.load("tests.crescent.plugins.plugin")
+        del bot.plugins.plugins[real_plugin.name]
+        orig2 = bot.plugins.load("tests.crescent.plugins.plugin")
+        new = bot.plugins.load("tests.crescent.plugins.plugin", refresh=True)
 
-    orig = bot.plugins.load("tests.crescent.plugins.plugin")
-    del bot.plugins.plugins[real_plugin.name]
-    orig2 = bot.plugins.load("tests.crescent.plugins.plugin")
-    new = bot.plugins.load("tests.crescent.plugins.plugin", refresh=True)
-
-    assert orig is orig2
-    assert orig is not new
+        assert orig is orig2
+        assert orig is not new
