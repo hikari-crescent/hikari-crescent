43c1371087c84a73012b7fc3dae7d0427ba55ee4
Circuit <circuitsacul@icloud.com>
2022-03-31 15:30:05 -0400

    Update __init__.py to reflect last commit

--- crescent/utils/__init__.py
+++ crescent/utils/__init__.py
@@ -4,7 +4,6 @@ from typing import Sequence
 
 from crescent.utils.gather_iter import *
 from crescent.utils.get_sig import *
-from crescent.utils.iterate_vars import *
 from crescent.utils.options import *
 
-__all__: Sequence[str] = ("iterate_vars", "gather_iter", "unwrap", "map_or", "get_parameters")
+__all__: Sequence[str] = ("gather_iter", "unwrap", "map_or", "get_parameters")
