283cf98e42fb4b5da997a844d412dd9deb8b5838
Circuit <circuitsacul@icloud.com>
2022-05-30 16:52:06 -0400

    Update basic error handling example

--- examples/error_handling/basic.py
+++ examples/error_handling/basic.py
@@ -1,17 +1,15 @@
+from re import I
+from typing import List
+
+import hikari
+from typing_extensions import Annotated as Atd
+
 import crescent
 
-bot = crescent.Bot("...")
+bot = crescent.Bot("OTQ1MTUwMTc1NjYyNDU2ODQy.YhL9sQ.OjXVlFpK-136l225DbtCvA0onC8")
 
 
 class RandomError(Exception):
-    pass
-
-
-class RandomError2(Exception):
-    pass
-
-
-class RandomError3(Exception):
     pass
 
 
@@ -19,28 +17,74 @@ class UnhandledError(Exception):
     pass
 
 
+# error handling
 @bot.include
-@crescent.catch(RandomError)
-async def on_random_error(exc: RandomError, ctx: crescent.Context) -> None:
+@crescent.catch_command(RandomError)
+async def on_cmd_random_error(exc: RandomError, ctx: crescent.Context) -> None:
     await ctx.respond(f"{exc} raised in {ctx.command}!")
 
 
 @bot.include
-@crescent.catch(RandomError2, RandomError3)
-async def on_random_error_2(exc: Exception, ctx: crescent.Context) -> None:
-    await ctx.respond(f"{exc} raised in {ctx.command}!")
+@crescent.catch_event(RandomError)
+async def on_event_random_error(exc: RandomError, event: hikari.Event) -> None:
+    print(f"{exc} raised in {event}!")
+
+
+@bot.include
+@crescent.catch_autocomplete(RandomError)
+async def on_autocomplete_random_error(
+    exc: RandomError, ctx: crescent.Context, inter: hikari.AutocompleteInteractionOption
+) -> None:
+    print(f"{exc} raised in autocomplete for {ctx.command}!")
+
+
+# buggy command/event/autocompletes
+@bot.include
+@crescent.command
+async def raise_error_cmd(ctx: crescent.Context, unhandled: bool) -> None:
+    if unhandled:
+        raise UnhandledError("Unhandled error!")
+    raise RandomError("Handled error")
+
+
+@bot.include
+@crescent.event
+async def raise_error_event(event: hikari.MessageCreateEvent) -> None:
+    if event.author.is_bot:
+        return
+    if event.message.content is None:
+        return
+
+    if event.message.content == "!unhandled":
+        raise UnhandledError("Unhandled error!")
+    elif event.message.content == "!error":
+        raise RandomError("Handled error")
+    elif event.message.content.startswith("!"):
+        await event.message.respond("Use !unhandled or !error")
+
+
+async def autocomplete(
+    ctx: crescent.Context, option: hikari.AutocompleteInteractionOption
+) -> List[hikari.CommandChoice]:
+    assert isinstance(option.value, str)
+    if option.value == "unhandled":
+        raise UnhandledError("Unhandled error!")
+    elif option.value == "error":
+        raise RandomError("Handled error")
+
+    return [
+        hikari.CommandChoice(name="error", value="error"),
+        hikari.CommandChoice(name="unhandled", value="unhandled"),
+    ]
 
 
 @bot.include
 @crescent.command
-async def raise_error(ctx: crescent.Context):
-    raise RandomError("Lol")
-
-
-@bot.include
-@crescent.command
-async def raise_unhandled_error(ctx: crescent.Context):
-    raise UnhandledError("oops...")
+async def autocomplete_error(
+    ctx: crescent.Context,
+    option: Atd[str, "Type error to error out", crescent.Autocomplete(autocomplete)],
+) -> None:
+    await ctx.respond(f"{option} (type unhandled or error inside option)")
 
 
 bot.run()
