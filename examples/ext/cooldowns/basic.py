5b885654826a0511cc09d798d754a94737c7ec05
Lunarmagpie <Bambolambo0@gmail.com>
2022-06-01 15:09:29 -0400

    example text

--- examples/ext/cooldowns/basic.py
+++ examples/ext/cooldowns/basic.py
@@ -12,6 +12,7 @@ async def my_command(ctx: crescent.Context) -> None:
     await ctx.respond("Hello!")
 
 
+# Callbacks can be set to run when a user is ratelimited
 async def on_rate_limited(ctx: crescent.Context, time_remaining: float) -> None:
     await ctx.respond(f"You are ratelimited for {time_remaining}s.")
 
