88b8968d3c2960a212129d1bef208d585b01b21a
Circuit <circuitsacul@icloud.com>
2022-06-08 22:23:31 -0400

    Add load/unload hooks to test bot plugin

--- tests/test_bot/test_plugin.py
+++ tests/test_bot/test_plugin.py
@@ -17,3 +17,13 @@ plugin = crescent.Plugin("plugin", [plugin_hook])
 @crescent.command
 async def plugin_cmd(ctx: crescent.Context) -> None:
     await ctx.respond("plugins work")
+
+
+@plugin.load_hook
+def on_load() -> None:
+    print("Plugin loaded")
+
+
+@plugin.unload_hook
+def on_unload() -> None:
+    print("Plugin unloaded")
