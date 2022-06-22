8ce383eeb7cdd766907c88eac7f062d6b157dfeb
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2022-06-22 16:09:57 -0500

    Remove incorrect use of TypeVar

--- crescent/commands/signature.py
+++ crescent/commands/signature.py
@@ -45,10 +45,7 @@ def _unwrap_optional(origin: type[Any]) -> Any:
     return args[1]
 
 
-def _get_arg(
-    t: type[Arg] | type[Any], metadata: Iterable[Any]
-) -> T | None:  # pyright: ignore # reportInvalidTypeVarUse
-    data: T
+def _get_arg(t: type[Arg] | type[Any], metadata: Iterable[Any]) -> Any | None:
     for data in metadata:
         if isinstance(data, t):
             return getattr(data, "payload", data)
