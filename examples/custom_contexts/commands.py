46b2e269bd9d3efb95ad7d9d45f131aaf76ccd44
Lunarmagpie <Bambolambo0@gmail.com>
2022-08-21 22:05:42 -0400

    add CustomContext2 example

--- examples/custom_contexts/commands.py
+++ examples/custom_contexts/commands.py
@@ -6,21 +6,25 @@ import crescent
 
 bot = crescent.Bot("...")
 
-# All custom contexts must inherit from `BaseContext``
+# All custom contexts must inherit from `crescent.BaseContext` or a subclass, such
+# as `crescent.Context`.
 # `BaseContext` does not define any methods. It only stores data from the interaction.
 class CustomContext(crescent.BaseContext):
     def a_custom_method(self) -> str:
         return "Hello there"
+
+class CustomContext2(crescent.Context):
+    ...
 
 
 @bot.include
 @crescent.command
 # To use a custom context, simply use the context object you want as the type hint.
 async def my_command(ctx: CustomContext) -> None:
-    # The custom method can now be called
+    # The custom method can now be called.
     message = ctx.a_custom_method()
 
-    # The type of a context can changed with the `BaseContext.into` function
+    # The type of a context can changed with the `BaseContext.into` method.
     crescent_ctx: crescent.Context = ctx.into(crescent.Context)
 
     # Methods on `crescent.Context` can now be used.
