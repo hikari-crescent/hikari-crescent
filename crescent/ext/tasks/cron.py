bf0cf9876b29eccec9cfdf57342609d730c186c0
Lunarmagpie <bambolambo0@gmail.com>
2022-09-18 22:45:36 -0400

    add the changes

--- crescent/ext/tasks/cron.py
+++ crescent/ext/tasks/cron.py
@@ -10,24 +10,32 @@ __all__: Sequence[str] = ("cronjob", "Cronjob")
 
 
 class Cronjob(Task):
-    def __init__(self, cron: str, callback: TaskCallbackT) -> None:
+    def __init__(self, cron: str, callback: TaskCallbackT, *, on_start: bool) -> None:
         self.cron: croniter = croniter(cron, datetime.now())
+        self.first_loop = on_start
 
         super().__init__(callback)
 
     def _next_iteration(self) -> float:
+        if self.first_loop:
+            return 0
+
         call_next_at: datetime = self.cron.get_next(datetime)
         time_to_next = call_next_at - datetime.now()
         return time_to_next.total_seconds()
 
+    def _call_next(self) -> None:
+        super()._call_next()
+        self.first_loop = False
 
-def cronjob(cron: str, /) -> Callable[[TaskCallbackT], Includable[Cronjob]]:
+
+def cronjob(cron: str, /, on_start: bool=False) -> Callable[[TaskCallbackT], Includable[Cronjob]]:
     """
     Run a task at the time specified by the cron schedule expression.
     """
 
     def inner(callback: TaskCallbackT) -> Includable[Cronjob]:
-        includable = Includable(Cronjob(cron, callback))
+        includable = Includable(Cronjob(cron, callback, on_start=on_start))
         Cronjob._link(includable)
         return includable
 
