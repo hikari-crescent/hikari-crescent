27513b5f85ead8988e211d41516811711f684d95
Lunarmagpie <Bambolambo0@gmail.com>
2022-09-27 00:00:28 -0400

    change order of tests

--- tests/crescent/utils/test_any_issubclass.py
+++ tests/crescent/utils/test_any_issubclass.py
@@ -13,10 +13,13 @@ def test_any_issubclass_with_any():
     assert not any_issubclass(None, B)
 
 
+def test_any_issubclass_with_object():
+    assert not any_issubclass(object(), B)
+
+
 def test_any_issubclass_with_cls():
     assert any_issubclass(B, A)
 
 
 def test_any_issubclass_with_cls_false():
     assert not any_issubclass(A, B)
-    assert not any_issubclass(object(), A)
