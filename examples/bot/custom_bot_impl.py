afad85d77a2c53cb3bb4d8e35af7ede6d42ee282
Lunarmagpie <bambolambo0@gmail.com>
2022-10-18 15:22:09 -0400

    add more warnings

--- examples/bot/custom_bot_impl.py
+++ examples/bot/custom_bot_impl.py
@@ -5,7 +5,8 @@ import hikari
 # The bot impl must have the traits `RESTAware` and `EventManagerAware`.
 # `CacheAware` is optional but supporting it supports more features.
 # NOTE: `crescent.Mixin` must be first.
-# NOTE: The `token` kwarg in the bot impl must be called `token`.
+# NOTE: The `token` kwarg in the bot impl must be called `token`. If this is
+# not the case a custom __init__ function is required.
 class Bot(crescent.Mixin, hikari.GatewayBot):
     ...
 
