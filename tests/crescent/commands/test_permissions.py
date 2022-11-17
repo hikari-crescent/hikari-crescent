a39fa64a3246c1959b990af064c820fc21d5ff83
Lunarmagpie <Bambolambo0@gmail.com>
2022-11-17 00:43:10 -0500

    :bug: Move permissions to `crescent.Group` (#263)
    * declare permissions in `crescent.Group`
    
    * add test

---
+++ tests/crescent/commands/test_permissions.py
@@ -0,0 +1,50 @@
+from crescent import command, Group, Context, PermissionsError
+from pytest import raises
+from hikari import PermissionOverwrite
+
+
+def test_no_perms_in_subcommand_group():
+    group = Group("abcd")
+
+    with raises(PermissionsError):
+
+        @group.child
+        @command(dm_enabled=False)
+        async def _command(ctx: Context) -> None:
+            ...
+
+    with raises(PermissionsError):
+
+        @group.child
+        @command(default_member_permissions=PermissionOverwrite(id=0, type=0))
+        async def _command(ctx: Context) -> None:
+            ...
+
+    @group.child
+    @command
+    async def _command(ctx: Context) -> None:
+        ...
+
+
+def test_no_perms_in_subcommand_subgroup():
+    group = Group("abcd")
+    sub_group = group.sub_group("abcd")
+
+    with raises(PermissionsError):
+
+        @sub_group.child
+        @command(dm_enabled=False)
+        async def _command(ctx: Context) -> None:
+            ...
+
+    with raises(PermissionsError):
+
+        @sub_group.child
+        @command(default_member_permissions=PermissionOverwrite(id=0, type=0))
+        async def _command(ctx: Context) -> None:
+            ...
+
+    @sub_group.child
+    @command
+    async def _command(ctx: Context) -> None:
+        ...
