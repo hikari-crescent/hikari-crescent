f4b501ba74e58ad48d40743597fa5103dfa4a792
Lunarmagpie <bambolambo0@gmail.com>
2023-01-20 10:23:13 -0500

    fix examples

--- examples/ext/cooldowns/basic.py
+++ examples/ext/cooldowns/basic.py
@@ -1,3 +1,5 @@
+import datetime
+
 import hikari
 
 import crescent
@@ -9,19 +11,19 @@ client = crescent.Client(bot)
 
 # The user can use the command 3 times in 20 seconds.
 @client.include
-@crescent.hook(cooldowns.cooldown(3, 20))
+@crescent.hook(cooldowns.cooldown(3, datetime.timedelta(seconds=20)))
 @crescent.command
 async def my_command(ctx: crescent.Context) -> None:
     await ctx.respond("Hello!")
 
 
 # Callbacks can be set to run when a user is ratelimited.
-async def on_rate_limited(ctx: crescent.Context, time_remaining: float) -> None:
+async def on_rate_limited(ctx: crescent.Context, time_remaining: datetime.timedelta) -> None:
     await ctx.respond(f"You are ratelimited for {time_remaining}s.")
 
 
 @client.include
-@crescent.hook(cooldowns.cooldown(1, 100, callback=on_rate_limited))
+@crescent.hook(cooldowns.cooldown(1, datetime.timedelta(seconds=100), callback=on_rate_limited))
 @crescent.command
 async def my_command2(ctx: crescent.Context) -> None:
     await ctx.respond("Hello!")
