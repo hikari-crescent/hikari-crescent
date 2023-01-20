99d47c698c4331b83da37bf3b145d090a934297a
Lunarmagpie <bambolambo0@gmail.com>
2023-01-20 08:23:41 -0500

    support differnt callback type for regular and after

--- crescent/hooks.py
+++ crescent/hooks.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any, Generic, Protocol, Sequence, TypeVar, overload
+from typing import TYPE_CHECKING, Any, Generic, Literal, Protocol, Sequence, TypeVar, overload
 
 if TYPE_CHECKING:
     from crescent.internal.app_command import AppCommandMeta
@@ -11,10 +11,23 @@ __all__: Sequence[str] = ("HookResult", "hook", "add_hooks")
 
 
 T = TypeVar("T", contravariant=True)
+U = TypeVar("U", contravariant=True)
 
 
-class Hookable(Protocol[T]):
-    def add_hooks(self, hooks: Sequence[T], prepend: bool = False, *, after: bool) -> None:
+class Hookable(Protocol[T, U]):
+    @overload
+    def add_hooks(
+        self, hooks: Sequence[T], prepend: bool = False, *, after: Literal[False]
+    ) -> None:
+        ...
+
+    @overload
+    def add_hooks(
+        self, hooks: Sequence[U], prepend: bool = False, *, after: Literal[True]
+    ) -> None:
+        ...
+
+    def add_hooks(self, hooks: Sequence[T | U], prepend: bool = False, *, after: bool) -> None:
         ...
 
 
@@ -23,7 +36,7 @@ class HookResult:
     exit: bool = False
 
 
-class hook(Generic[T]):
+class hook(Generic[T, U]):
     def __init__(self, *callbacks: T, after: bool = False):
         self.callbacks = callbacks
         self.after = after
@@ -35,7 +48,7 @@ class hook(Generic[T]):
         ...
 
     @overload
-    def __call__(self, includable: Includable[Hookable[T]]) -> Includable[Hookable[T]]:
+    def __call__(self, includable: Includable[Hookable[T, U]]) -> Includable[Hookable[T, U]]:
         ...
 
     def __call__(self, includable: Includable[Any]) -> Includable[Any]:
@@ -44,10 +57,10 @@ class hook(Generic[T]):
 
 
 def add_hooks(
-    includable: Includable[Hookable[T]],
+    includable: Includable[Hookable[T, U]],
     *,
     hooks: Sequence[T] | None,
-    after_hooks: Sequence[T] | None,
+    after_hooks: Sequence[U] | None,
 ) -> None:
     if hooks:
         includable.metadata.add_hooks(hooks, after=False)
