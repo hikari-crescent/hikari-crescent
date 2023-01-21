9e22ba03833c7be2879d4291c002e3b17859497b
CircuitSacul <circuitsacul@icloud.com>
2023-01-20 21:51:08 -0500

    add autocomplete to rest example

--- examples/rest/basic.py
+++ examples/rest/basic.py
@@ -1,20 +1,39 @@
+from __future__ import annotations
+
 import asyncio
+import random
 
-from hikari import RESTBot, TokenType
+from hikari import RESTBot, TokenType, CommandChoice, AutocompleteInteractionOption
 
-from crescent import Client, command, Context
+import crescent
 
 
-bot = RESTBot("...", TokenType.BOT)
-client = Client(bot)
+bot = RESTBot("OTgzOTExMDgwMjkwMDM3Nzkx.GssPTP.0qgNmazCATq0gS1ex22NBi-CayLG_e_VN8BI5w", TokenType.BOT)
+client = crescent.Client(bot)
+
+
+async def autocomplete_callback(ctx: crescent.AutocompleteContext, option: AutocompleteInteractionOption) -> list[CommandChoice]:
+    chars = [c for c in str(option.value)]
+    random.shuffle(chars)
+    shuffled = "".join(chars)
+    value = f"Showing results for '{shuffled}'."
+    return [CommandChoice(name=value, value=shuffled)]
 
 
 @client.include
-@command(name="ping", description="Pong!")
-async def ping(ctx: Context) -> None:
+@crescent.command(name="ping", description="Pong!")
+async def ping(ctx: crescent.Context) -> None:
     await ctx.defer()
     await asyncio.sleep(2)
     await ctx.respond("heyo")
 
+
+@client.include
+@crescent.command(name="autcomplete", description="Autocomplete Test")
+class AutocompleteTest:
+    option = crescent.option(str, "An option", autocomplete=autocomplete_callback)
+
+    async def callback(self, ctx: crescent.Context) -> None:
+        await ctx.respond(f"You said {self.option}!")
 
 bot.run()
