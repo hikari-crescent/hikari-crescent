b82a55385a4d65baba49255d4bf507bd4aec2a49
Lunarmagpie <bambolambo0@gmail.com>
2023-02-08 21:56:29 -0500

    update CI to use dependency groups

--- noxfile.py
+++ noxfile.py
@@ -1,29 +1,34 @@
 from __future__ import annotations
 
+import functools
+import os
 import typing
 
 import nox
 
 
 def poetry_session(
-    callback: typing.Callable[[nox.Session], None]
-) -> typing.Callable[[nox.Session], None]:
-    @nox.session(name=callback.__name__)
-    def inner(session: nox.Session) -> None:
-        session.install("poetry")
-        session.run("poetry", "shell")
-        session.run("poetry", "install", "--all-extras")
-        callback(session)
+    *groups: str, extras: bool = False, **kwargs: typing.Any
+) -> typing.Callable[[typing.Callable[[nox.Session], None]], typing.Callable[[nox.Session], None]]:
+    def inner(
+        callback: typing.Callable[[nox.Session], None]
+    ) -> typing.Callable[[nox.Session], None]:
+        @nox.session(**kwargs, reuse_venv=bool(os.environ.get("CI")))
+        @functools.wraps(callback)
+        def inner(session: nox.Session) -> None:
+            session.install("poetry")
+            session.run("poetry", "shell")
 
-    return inner
+            command = ["poetry", "install"]
 
+            if groups:
+                command += ["--with=" + ",".join(groups)]
 
-def pip_session(*args: str, name: str | None = None) -> typing.Callable[[nox.Session], None]:
-    def inner(callback: typing.Callable[[nox.Session], None]):
-        @nox.session(name=name or callback.__name__)
-        def inner(session: nox.Session):
-            for arg in args:
-                session.install(arg)
+            if extras:
+                command += ["--all-extras"]
+
+            session.run(*command)
+
             callback(session)
 
         return inner
@@ -31,14 +36,14 @@ def pip_session(*args: str, name: str | None = None) -> typing.Callable[[nox.Ses
     return inner
 
 
-@pip_session("black", "isort", "codespell", name="apply-lint")
+@poetry_session("linting", name="format")
 def apply_lint(session: nox.Session) -> None:
     session.run("black", "crescent")
     session.run("isort", "crescent")
     session.run("codespell", "crescent", "-i", "2")
 
 
-@pip_session("black", "codespell", "ruff", "isort")
+@poetry_session("linting")
 def lint(session: nox.Session) -> None:
     session.run("black", "--check", "crescent")
     session.run("codespell", "crescent")
@@ -46,23 +51,23 @@ def lint(session: nox.Session) -> None:
     session.run("isort", "--check", "crescent")
 
 
-@poetry_session
+@poetry_session("typing", extras=True)
 def mypy(session: nox.Session) -> None:
     session.run("poetry", "run", "mypy", "crescent")
 
 
-@poetry_session
+@poetry_session("typing", extras=True)
 def pyright(session: nox.Session) -> None:
     session.run("poetry", "run", "pyright")
     session.run("poetry", "run", "pyright", "--verifytypes", "crescent")
 
 
-@poetry_session
+@poetry_session("tests")
 def pytest(session: nox.Session) -> None:
     session.install("hikari[server]")
     session.run("poetry", "run", "pytest", "tests/crescent", "--cov=crescent/")
 
 
-@poetry_session
+@poetry_session("doc")
 def docs(session: nox.Session) -> None:
     session.run("pdoc", "crescent", "-d", "google", "-o", "docs/_build")
