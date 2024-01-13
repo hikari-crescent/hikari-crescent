889a4b79c311d177af1bf5a4ac0f3810b91bf6f5
Lunarmagpie <bambolambo0@gmail.com>
2024-01-13 15:29:35 -0500

    remove autodefer

--- crescent/ext/defertools/__init__.py
+++ crescent/ext/defertools/__init__.py
@@ -1,17 +1,26 @@
 from __future__ import annotations
 
-from asyncio import sleep
 from collections.abc import Awaitable
-from datetime import timedelta
 from typing import Callable, overload
 
 from crescent import Context
-from crescent.utils import create_task
 
-__all__ = ["defer", "autodefer"]
+__all__ = ["defer"]
 
 
-async def defer(ctx: Context) -> None:
+@overload
+def defer(*, ephemeral: bool = ...) -> Callable[[Context], Awaitable[None]]:
+    pass
+
+
+@overload
+def defer(ctx: Context) -> Awaitable[None]:
+    pass
+
+
+def defer(
+    ctx: Context | None = None, *, ephemeral: bool = False
+) -> Callable[[Context], Awaitable[None]] | Awaitable[None]:
     """
     Hook used to defer a task.
 
@@ -29,78 +38,8 @@ async def defer(ctx: Context) -> None:
         await ctx.respond("Hello world!!")
     ```
     """
-    await ctx.defer()
+    if ctx is not None:
+        return ctx.defer(ephemeral=ephemeral)
 
+    return lambda ctx: ctx.defer(ephemeral=ephemeral)
 
-async def _noop() -> None:
-    pass
-
-
-@overload
-def autodefer(*, time: timedelta | None = None) -> Callable[[Context], Awaitable[None]]:
-    ...
-
-
-@overload
-def autodefer(ctx: Context, *, time: timedelta | None = None) -> Awaitable[None]:
-    ...
-
-
-def autodefer(
-    ctx: Context | None = None, *, time: timedelta | None = None
-) -> Awaitable[None] | Callable[[Context], Awaitable[None]]:
-    """
-    Hook used to defer tasks automatically, by default after two seconds.
-
-    ```python
-    from crescent.ext.defer import autodefer
-
-    @client.include
-    @crescent.hook(autodefer)
-    @crescent.command
-    async def command(ctx: crescent.Context) -> None:
-        # Simulate a long task...
-        await asyncio.sleep(10)
-
-        await ctx.respond("Hello world!!")
-    ```
-
-    You can use this command hook globally for autodefer on your entire bot.
-
-    ```python
-    import hikari
-    import crescent
-    from crescent.ext.defer import autodefer
-
-    bot = hikari.GatewayBot(...)
-    client = crescent.Client(bot, command_hooks=[autodefer])
-    ```
-
-    Args:
-        time:
-            The time to wait before automatically deferring the task. By default
-            this is two seconds.
-    """
-    if not ctx:
-
-        async def partial(ctx: Context) -> None:
-            return await autodefer(ctx, time=time)
-
-        return partial
-
-    # bool(timedelta()) is False
-    if time is None:
-        time = timedelta(seconds=2)
-
-    async def task() -> None:
-        assert ctx
-        assert time is not None
-        await sleep(time.total_seconds())
-        if ctx._has_deferred_response or ctx._has_created_response:
-            return
-        else:
-            await ctx.defer()
-
-    create_task(task())
-
-    return _noop()
