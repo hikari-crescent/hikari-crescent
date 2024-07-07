064afe45015f8f5c8c6202eed77dd4512f9bb361
Lunarmagpie <Bambolambo0@gmail.com>
2024-07-07 15:24:38 -0400

    Update Dependencies (#356)
    Update all of Pyright, Pytest, and Hikari dependencies.
    - Fixes a bug where pyright believes types wrongly overlap.
    - Adds support for registered_guild_id and entitlements to crescent.
    - Removes pytest-cov. It was broken and unused.

--- tests/crescent/ctx/test_ctx.py
+++ tests/crescent/ctx/test_ctx.py
@@ -21,9 +21,11 @@ def test_into():
         group=16,
         sub_group=17,
         options=18,
-        _has_created_response=19,
-        _has_deferred_response=20,
-        _rest_interaction_future=21,
+        registered_guild_id=19,
+        entitlements=20,
+        _has_created_response=21,
+        _has_deferred_response=22,
+        _rest_interaction_future=23,
     )
 
     ctx2 = ctx.into(InteractionContext)
@@ -46,6 +48,8 @@ def test_into():
     assert ctx.group == ctx2.group
     assert ctx.sub_group == ctx2.sub_group
     assert ctx.options == ctx2.options
+    assert ctx.registered_guild_id == ctx2.registered_guild_id
+    assert ctx.entitlements == ctx2.entitlements
     assert ctx._has_created_response == ctx2._has_created_response
     assert ctx._has_deferred_response == ctx2._has_deferred_response
     assert ctx._rest_interaction_future == ctx2._rest_interaction_future
