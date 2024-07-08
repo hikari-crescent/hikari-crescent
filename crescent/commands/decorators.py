774c51e4b5e6982d537db2e75bc3c00ed38febc9
ELJ <47494925+EllieJaybee@users.noreply.github.com>
2024-07-09 01:41:42 +0800

    Fixed hyperlink bracket mixup (#355)

--- crescent/commands/decorators.py
+++ crescent/commands/decorators.py
@@ -107,8 +107,8 @@ def command(
             command will be registered globally.
         default_member_permissions:
             The default permissions for this command. For more information see
-            (the discord api docs)[https://discord.com/developers/docs/topics/permissions]
-            and (the hikari docs)[https://docs.hikari-py.dev/en/latest/reference/hikari/permissions/].
+            [the discord api docs](https://discord.com/developers/docs/topics/permissions)
+            and [the hikari docs](https://docs.hikari-py.dev/en/latest/reference/hikari/permissions/).
         dm_enabled:
             Set to `False` to disable in dms. Defaults to `True`.
         nsfw:
@@ -320,8 +320,8 @@ def message_command(
             command will be registered globally.
         default_member_permissions:
             The default permissions for this command. For more information see
-            (the discord api docs)[https://discord.com/developers/docs/topics/permissions]
-            and (the hikari docs)[https://docs.hikari-py.dev/en/latest/reference/hikari/permissions/].
+            [the discord api docs](https://discord.com/developers/docs/topics/permissions)
+            and [the hikari docs](https://docs.hikari-py.dev/en/latest/reference/hikari/permissions/).
         dm_enabled:
             Set to `False` to disable in dms. Defaults to `True`.
         nsfw:
